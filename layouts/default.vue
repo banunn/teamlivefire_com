512c14a1e98c2579077ff90f5a8fdd41b45a036b
banunn <brock@brocknunn.com>
2021-08-23 12:28:34 -0500

    Initial Commit

---
+++ layouts/default.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app light>
+    <v-app-bar flat clipped-left="clipped" fixed app>
+      <v-spacer />
+      <v-btn to="/support">Support</v-btn>
+      <v-btn color="secondary" class="ml-4">Log In</v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
---
+++ pages/support.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row>
+    <h1 class="text-center" v-text="count"></h1>
+    <v-btn @click="count += 1">Increment</v-btn>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'support_page',
+  data: () => ({
+    count: 3,
+  }),
+}
+</script>
+
+<style lang="scss" scoped></style>
