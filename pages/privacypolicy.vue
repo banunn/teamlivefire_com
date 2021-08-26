f65f4e1ddb2e43f419ba56e3aadc20d1f0c3d0f4
banunn <brock@brocknunn.com>
2021-08-26 10:59:41 -0500

    cms

---
+++ pages/privacypolicy.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'privacy_policy',
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/termsandconditions.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'terms_page',
+}
+</script>
+
+<style lang="scss" scoped></style>
